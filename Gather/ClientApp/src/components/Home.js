153d522ea3c2f7343ae6915d4fbc0b3147bbe954
Somya Upadhyay <23.7saumya@gmail.com>
2020-11-10 15:30:23 +0530

    Gather0: About section

--- Gather/ClientApp/src/components/Home.js
+++ Gather/ClientApp/src/components/Home.js
@@ -6,20 +6,16 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>Hello, Microsoft world!</h1>
+        <p>Welcome to <b>Gather</b>. This space aims at enabling people to enjoy the joy of giving! So, post what you have to offer, see what others have to share!</p>
+        <p></p>
+        <p>Built with love and much fun by: </p>
         <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li><a href=''>Somya Upadhay</a></li>
+          <li><a href=''>Sonal Verma</a></li>
+          <li><a href=''>Sachin Gupta</a></li>
         </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <p><code>The meaning of life is to find your gift, the purpose of life is to give it away. - William Shakespeare</code></p>
       </div>
     );
   }
